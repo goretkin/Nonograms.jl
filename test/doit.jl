7415b943327758f452d069bde965ce2391f8f174
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-05-06 22:11:17 -0400

    Test a puzzle

--- test/doit.jl
+++
@@ -1,8 +0,0 @@
-import Nonograms
-import Nonograms: solve_nonogram, make_string
-
-include("nancy_drew_booklet.jl")
-image = solve_nonogram(row_margins, column_margins)
-
-println("\n\nresult:")
-println(make_string(image))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,23 @@
 using Nonograms
 using Test
-import Nonograms: run_length_encode
+using Nonograms: run_length_encode, solve_nonogram
+
+module Puzzle1
+    include("nancy_drew_booklet.jl")
+end
+
+module Puzzle2
+    include("nancy_drew_lastnono.jl")
+end
+
 @testset "RLE" begin
     @test 0 == length(run_length_encode([]))
     @test [(0, 2), (1, 1), (0, 3)] == run_length_encode([0,0,1,0,0,0])
+end
+
+@testset "puzzles" begin
+    for P in [Puzzle1, #=Puzzle2=#] #  Puzzle2 is kind of slow with the naive solver
+        image = solve_nonogram(P.row_margins, P.column_margins)
+        @test image == P.solution
+    end
 end
